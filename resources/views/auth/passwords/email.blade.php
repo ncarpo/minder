cc2789222205ed728237c34e99df047e9b93b77b
Unknown <neil@impelling.co.uk>
2018-11-08 22:52:21 +0000

    Remove default markup for email reset view

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+    <p>{{ __('Reset Password') }}</p>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    @if (session('status'))
+        {{ session('status') }}
+    @endif
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+    <form method="POST" action="{{ route('password.email') }}">
+        @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+        <label for="email">{{ __('E-Mail Address') }}</label>
+        <input id="email" type="email" name="email" value="{{ old('email') }}" required>
+        @if ($errors->has('email'))
+            <p>{{ $errors->first('email') }}</p>
+        @endif
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+        <button type="submit">{{ __('Send Password Reset Link') }}</button>
+    </form>
 @endsection
