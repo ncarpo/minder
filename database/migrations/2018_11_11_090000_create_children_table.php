2784cc213926166053b25c77cd2c4d5f42ee60ab
Unknown <neil@impelling.co.uk>
2018-11-11 09:29:11 +0000

    Add child model and migration

---
+++ database/migrations/2018_11_11_090000_create_children_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChildrenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('children', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('minder_id');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->datetime('dob');
+            $table->timestamps();
+            $table->foreign('minder_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('children');
+    }
+}
